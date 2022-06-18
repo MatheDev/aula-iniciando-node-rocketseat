db25e900bf1bcaa39f14018526229fa1b9e40fab
Matheus Tomaz <matheusdeveloperr@gmail.com>
2022-06-18 15:48:35 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,25 @@
+const os = require('os')     //Common Js utilizado para importar modulos
+
+
+
+
+// Responsável por mandar o código todo abaixo a cada 1 segundo, onde vai ser atualizado
+setInterval(() => {
+    const { freemem, totalmem} = os // desistruturação do objeto, extraindo de dentro do os
+
+    const total = parseInt(totalmem() / 1024 / 1024) // total de memória livre formatando em MB
+    const mem = parseInt(freemem() / 1024 / 1024) // memória em formatando em MB
+    const percents = parseInt((mem / total) * 100) // criando a porcentagem
+
+    //formatando o resultado em um objeto
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents} %`
+    }
+
+    console.clear() //Limpa o console antes de mostrar a mensagem
+    console.log("=== PC STATS ===");
+    console.table(stats)//mostra o resultado em forma de tabela
+
+}, 1000)
